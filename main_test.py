2668bf890df95c74c0685095ccc63d1cc9275bc4
dauletb01 <dauletb01@gmail.com>
2024-05-31 16:49:58 +0500

    first commit

--- main_test.py
+++ main_test.py
@@ -6,6 +6,7 @@ config_file_name = "config.json"
 task_file_name = "task.json"
 
 
+
 def get_config():
 
     with open(config_file_name, "r", encoding="utf-8") as file:
@@ -36,6 +37,22 @@ def get_task():
 
     return task_data
 
+def get_amaunt_task():
+    tasks = get_task()
+    amaunt_done = 0
+    amaunt_progres = 0
+
+    for task in tasks:
+        if task["status"]: 
+            amaunt_done+=1
+        else:
+            amaunt_progres+=1
+
+    return amaunt_done, amaunt_progres
+
+
+
+
 def write_task(task_data):
 
     with open(task_file_name, "w", encoding="utf-8") as file:
@@ -52,19 +69,50 @@ def checkbutton_changed(id):
             break
     
     write_task(tasks)
-    task_draw()
+    draw_task()
+    draw_status()
 
 
-def task_draw():
+def draw_task():
 
     for widget in frame_left.winfo_children():
         widget.destroy()
 
     tasks = get_task()
 
+    try: 
+        if status == "ALL": draw_task_all(tasks)
+        elif status == "done": draw_task_done(tasks)
+        elif status == "progres": draw_task_progres(tasks)
+    except NameError:
+        draw_task_all(tasks)
+        print("suka")
+    else:
+        print("Nothing went wrong")
+
+    
+
+def draw_task_all(tasks):
     i = 0
     for task in tasks:
         
+        enabled = IntVar()
+        number=tk.Label(frame_left, text=f"{task["id"]}", borderwidth=2, relief='solid',padx=10, pady=10)
+        text=tk.Label(frame_left, text=f"{task["task"]}", borderwidth=2, relief='solid',padx=10, pady=10, justify=tk.LEFT)
+        # status = tk.Checkbutton(frame_left, text= "Done" if task["status"] else "In progress" , borderwidth=2, variable=enabled, command=checkbutton_changed(task["id"]), relief='solid', padx=10, pady=10)
+        status = tk.Checkbutton(frame_left, text="Done" if task['status'] else "In progress", borderwidth=2, variable=enabled, command=lambda task_id=task['id']: checkbutton_changed(task_id), relief='solid', padx=10, pady=10)
+
+        number.grid(row=i, column=0, sticky='nsew')
+        text.grid(row=i, column=1, sticky='nsew')
+        status.grid(row=i, column=2, sticky='nsew')
+
+        i+=1
+
+def draw_task_done(tasks):
+    i = 0
+    for task in tasks:
+        if not(task["status"]): pass
+
         enabled = IntVar()
         number=tk.Label(frame_left, text=f"{task["id"]}", borderwidth=2, relief='solid',padx=10, pady=10)
         text=tk.Label(frame_left, text=f"{task["task"]}", borderwidth=2, relief='solid',padx=10, pady=10, justify=tk.LEFT)
@@ -77,6 +125,42 @@ def task_draw():
         status.grid(row=i, column=2, sticky='nsew')
 
         i+=1
+
+def draw_task_progres(tasks):
+    i = 0
+    for task in tasks:
+        if task["status"]: pass
+
+        enabled = IntVar()
+        number=tk.Label(frame_left, text=f"{task["id"]}", borderwidth=2, relief='solid',padx=10, pady=10)
+        text=tk.Label(frame_left, text=f"{task["task"]}", borderwidth=2, relief='solid',padx=10, pady=10, justify=tk.LEFT)
+        # status = tk.Checkbutton(frame_left, text= "Done" if task["status"] else "In progress" , borderwidth=2, variable=enabled, command=checkbutton_changed(task["id"]), relief='solid', padx=10, pady=10)
+        status = tk.Checkbutton(frame_left, text="Done" if task['status'] else "In progress", borderwidth=2, variable=enabled, command=lambda task_id=task['id']: checkbutton_changed(task_id), relief='solid', padx=10, pady=10)
+
+
+        number.grid(row=i, column=0, sticky='nsew')
+        text.grid(row=i, column=1, sticky='nsew')
+        status.grid(row=i, column=2, sticky='nsew')
+
+        i+=1
+
+def draw_status():
+    amaunt_done, amaunt_prigres = get_amaunt_task()
+
+    for widget in frame_right_status.winfo_children():
+        widget.destroy()
+
+
+    lable_done = tk.Label(frame_right_status, text=f"Done:", font=("Arial", 10, 'bold'))
+    lable_done.grid( row=0, column=0)
+    lable_done_count = tk.Label(frame_right_status, text= amaunt_done, font=("Arial", 10, 'bold'))
+    lable_done_count.grid( row=0, column=1)
+
+
+    lable_progres = tk.Label(frame_right_status, text=f"In progres:", font=("Arial", 10, 'bold'))
+    lable_progres.grid( row=1, column=0)
+    lable_progres_count = tk.Label(frame_right_status, text= amaunt_prigres, font=("Arial", 10, 'bold'))
+    lable_progres_count.grid( row=1, column=1)
 
 # --------------------------------
 
@@ -105,36 +189,44 @@ frame_left.grid_columnconfigure(0, minsize=50,weight=1)
 frame_left.grid_columnconfigure(1, minsize=400,weight=10)
 frame_left.grid_columnconfigure(2, minsize=150,weight=1)
 
-task_draw()
+draw_task()
 
 
 
 frame_right = tk.Frame(root, borderwidth=2, relief='groove', width=200)
 frame_right.pack(side=tk.LEFT, fill=Y)
 
+# Создание кнопак
 frame_right_button = tk.Frame(frame_right, borderwidth=2, relief='groove', padx=10, pady=10, width=200)
 frame_right_button.pack(side=tk.TOP, fill=Y)
 
 add_button = tk.Button(frame_right_button, borderwidth=2, relief='groove', padx=5, pady=5, text="Add task", width=8, height=1)
 delate_button = tk.Button(frame_right_button, borderwidth=2, relief='groove', padx=5, pady=5, text="Delate task", width=8, height=1)
 
-
 add_button.pack(side=tk.LEFT)
 delate_button.pack(side=tk.LEFT)
 
 
+# Создание показании статус
 frame_right_status = tk.Frame(frame_right, borderwidth=2, relief='groove', padx=10, pady=10, width=200)
 frame_right_status.pack(side=tk.TOP, fill=X)
-
-lable_done = tk.Label(frame_right_status, text=f"Done:", font=("Arial", 10, 'bold'))
-lable_done.pack( side=tk.LEFT, fill=Y)
-lable_done_count = tk.Label(frame_right_status, text=f"2", font=("Arial", 10, 'bold'))
-lable_done_count.pack(side=tk.LEFT, fill=Y)
+draw_status()
 
 
-lable_progres = tk.Label(frame_right_status, text=f"In progres:", font=("Arial", 10, 'bold'))
-lable_progres.pack( side=tk.LEFT, fill=Y)
-lable_progres_count = tk.Label(frame_right_status, text=f"1", font=("Arial", 10, 'bold'))
-lable_progres_count.pack( side=tk.LEFT, fill=Y)
+# Создание выбора отображения
+frame_right_select = tk.Frame(frame_right, borderwidth=2, relief='groove', padx=10, pady=10, width=200)
+frame_right_select.pack(side=tk.TOP, fill=X)
+
+
+position = {"padx":6, "pady":6, "anchor":NW}
+status = StringVar(value="ALL")
+btn_all = tk.Radiobutton(frame_right_select, text="ALL", value="ALL", variable=status, command= draw_task())
+btn_all.pack(**position)
+  
+btn_done = tk.Radiobutton(frame_right_select, text="Done", value="done", variable=status, command= draw_task())
+btn_done.pack(**position)
+ 
+btn_progres = tk.Radiobutton(frame_right_select, text="Progres", value="progres", variable=status, command= draw_task())
+btn_progres.pack(**position)
 
 root.mainloop()
